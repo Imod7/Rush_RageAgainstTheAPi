599bc49b0459c1f74ad91696b9e6276883388c78
Dominique Saripapazoglou <dsaripap@f1r6s7.codam.nl>
2019-11-01 20:50:40 +0100

    JSON data extraction - Extracting information from fr_idf.json file - I can only find 7 lines out of 16, it seems the rest is not available in the file - I can print the metro lines, the line terminals and stop_areas

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import requests
 import json
+import colors as col
 
 myToken = 'ecc69723-9515-4a86-a91a-b370e7a9e369'
 # myUrl = 'https://api.navitia.io/v1/coverage'
+# myUrl = 'https://api.navitia.io/v1/coverage/fr-idf/pt_objects?q=RATP'
 myUrl = 'https://api.navitia.io/v1/coverage/fr-idf/pt_objects?q=metro'
 
 head = {'Authorization': myToken}
 response = requests.get(myUrl, headers=head)
 
+print(col.YELLOW + "Response Status Code : " + col.RESET)
 print(response.status_code)
 
 data = response.json()
 # print data['pt_objects'][0]['name']
 
-for element in data['pt_objects']:
+print(col.YELLOW + "\nMetro Line Names\n" + col.RESET)
+for element in data['pt_objects'][3:]:
     print(element['name'])
 
-print("\n")
 # print len(data['pt_objects'][3])
-
+print(col.YELLOW + "\nMetro Line - Terminus Names\n" + col.RESET)
 for obj in data['pt_objects']:
     if 'line' in obj:
         print(obj['line']['name'])
+        
+print(col.YELLOW + "\nMetro Stop Areas of Lines\n" + col.RESET)
+for obj in data['pt_objects']:
+    if 'line' in obj:
+        for obj1 in obj['line']['routes']:
+            print(obj1['direction']['stop_area']['label'])
 
-print("\n")
 # for element in data['pt_objects']:
 # #    print("Key: " + k)
 # #    print("Value: " + str(v))
